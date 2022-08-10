c3b902795359caa34deea405724cc2704d362840
JE668 <63463703+JE668@users.noreply.github.com>
2022-08-10 09:58:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 echo 'Modify default IP'
-sed -i 's/10.0.0.1/10.0.1.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 echo '修改主机名'
 sed -i "s/hostname='OpenWrt'/hostname='Phicomm-K3'/g" package/base-files/files/bin/config_generate
