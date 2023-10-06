e1cda2f6d00933ede6e70d88ae556333714d9492
JE668 <63463703+JE668@users.noreply.github.com>
2023-10-06 23:02:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -31,10 +31,10 @@
 #cat feeds.conf.default |grep bypass
 #echo '=========Add bypass source OK!========='
 
-echo '添加HelloWorld软件源'
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo '=========Add HelloWorld source OK!========='
+#echo '添加HelloWorld软件源'
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo '=========Add HelloWorld source OK!========='
 
 #echo '添加HelloWorld及依赖'
 #rm -rf package/lean/lua-maxminddb
@@ -48,10 +48,10 @@ echo '=========Add HelloWorld source OK!========='
 #git clone https://github.com/kiddin9/luci-app-dnsfilter package/lean/luci-app-dnsfilter 
 #echo '=========Add dnsfilter source OK!========='
 
-echo '添加AdguardHome'
-rm -rf package/lean/luci-app-adguardhome 
-git clone https://github.com/kongfl888/luci-app-adguardhome package/lean/luci-app-adguardhome 
-echo '=========Add AdguardHome OK!========='
+#echo '添加AdguardHome'
+#rm -rf package/lean/luci-app-adguardhome 
+#git clone https://github.com/kongfl888/luci-app-adguardhome package/lean/luci-app-adguardhome 
+#echo '=========Add AdguardHome OK!========='
 
 #echo '添加my-dnshelper'
 #rm -rf package/lean/luci-app-my-dnshelper
@@ -82,6 +82,10 @@ echo '=========Add AdguardHome OK!========='
 #rm -rf package/lean/luci-theme-neobird
 #git clone https://github.com/thinktip/luci-theme-neobird.git package/lean/luci-theme-neobird
 #echo '=========Add neobird-theme OK!========='
+
+echo '添加kiddin9软件源'
+sed -i '$a src-git openwrt_kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
+echo '=========Add kiddin9 source OK!========='
 
 echo '添加lwz322的K3屏幕插件'
 rm -rf package/lean/luci-app-k3screenctrl
