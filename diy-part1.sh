d7a25996eccb2add862f8a01a044ac21d786fcbb
JE668 <63463703+JE668@users.noreply.github.com>
2024-11-09 10:38:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,118 +1,94 @@
 #!/bin/bash
 #
-# Copyright (c) 2020-2024 JE668 <https://github.com/JE668>
+# Copyright (c) 2020-2024 JE <https://gihub.com/JE668>
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
-# File name: diy-part1.sh
+# https://github.com/JE668/Phicomm-K3-LEDE-Firmware-Lean
+# File name: beta/diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
 # Uncomment a feed source
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
-# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo '添加SSR-Plus软件源'
+sed -i "/helloworld/d" "feeds.conf.default"
+echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
+echo '=========Add SSR-Plus source OK!========='
 
-# echo '添加Passwall dependencies'
-# sed -i '$a src-git passpack https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-# echo '=========Add feed source OK!========='
+echo '添加passwall软件源'
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwallpackages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
+echo '=========Add passwall source OK!========='
 
-# echo '添加Passwall软件源'
-# rm -rf package/lean/luci-app-passwall 
-# git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/lean/luci-app-passwall 
-# echo '=========Add passwall source OK!========='
-
-# echo '添加bypass软件源'
-# sed -i '$a src-git bypass https://github.com/kiddin9/openwrt-bypass' feeds.conf.default
-# cat feeds.conf.default |grep bypass
-# echo '=========Add bypass source OK!========='
-
-# echo '添加kenzok8软件源'
-# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-# sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-# echo '=========Add kenzok8 source OK!========='
-
-# echo '添加HelloWorld软件源'
-# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-# echo '=========Add HelloWorld source OK!========='
-
-# echo '添加HelloWorld及依赖'
-# rm -rf package/lean/lua-maxminddb
-# git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
-# rm -rf package/lean/luci-app-vssr
-# git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
-# echo '=========Add HelloWorld & dependencies OK!========='
-
-# echo '添加dnsfilter'
-# rm -rf package/lean/luci-app-dnsfilter 
-# git clone https://github.com/kiddin9/luci-app-dnsfilter package/lean/luci-app-dnsfilter 
-# echo '=========Add dnsfilter source OK!========='
-
-# echo '添加AdguardHome'
+echo '添加AdguardHome'
 # rm -rf feeds/kenzo/adguardhome
 # rm -rf feeds/kenzo/luci-app-adguardhome
 # rm -rf package/feeds/kenzo/luci-app-adguardhome
+# rm -rf package/feeds/kenzo/adguardhome
 # rm -rf package/lean/luci-app-adguardhome
-# git clone https://github.com/kongfl888/luci-app-adguardhome package/lean/luci-app-adguardhome 
-# echo '=========Add AdguardHome OK!========='
-
-# echo '添加my-dnshelper'
-# rm -rf package/lean/luci-app-my-dnshelper
-# git clone https://github.com/kongfl888/openwrt-my-dnshelper/ package/lean/luci-app-my-dnshelper
-# echo '=========Add my-dnshelper OK!========='
-
-# echo '添加adbyby-plus-lite'
-# rm -rf package/lean/luci-app-adbyby-plus
-# git clone https://github.com/kongfl888/luci-app-adbyby-plus-lite package/lean/luci-app-adbyby-plus
-# echo '=========Add adbyby-plus-lite OK!========='
-
-# echo '添加OpenClash'
-# rm -rf package/lean/luci-app-openclash
-# svn checkout https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/lean/luci-app-openclash
-# echo '=========Add OpenClash source OK!========='
-
-# echo '添加ikoolproxy'
-# rm -rf package/lean/luci-app-ikoolproxy
-# git clone https://github.com/ilxp/luci-app-ikoolproxy.git package/lean/luci-app-ikoolproxy
-# echo '=========Add ikoolproxy source OK!========='
-
-# echo '添加jerrykuku的argon-mod主题'
-# rm -rf package/lean/luci-theme-argon  
-# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/lean/luci-theme-argon
-# echo '=========Add argon-mod OK!========='
-
-# echo '添加tinktip的neobird主题'
-# rm -rf package/lean/luci-theme-neobird
-# git clone https://github.com/thinktip/luci-theme-neobird.git package/lean/luci-theme-neobird
-# echo '=========Add neobird-theme OK!========='
-
-#echo '添加kiddin9软件源'
-#echo 'src-git openwrt_kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
-#echo '=========Add kiddin9 source OK!========='
+git clone https://github.com/kongfl888/luci-app-adguardhome package/lean/luci-app-adguardhome 
+echo '=========Add AdguardHome OK!========='
 
 echo '添加lwz322的K3屏幕插件'
 rm -rf package/lean/luci-app-k3screenctrl
-git clone https://github.com/je668/luci-app-k3screenctrl.git package/lean/luci-app-k3screenctrl
+git clone https://github.com/JE668/luci-app-k3screenctrl.git package/lean/luci-app-k3screenctrl
 echo '=========Add k3screen plug OK!========='
 
 echo '替换lwz322的K3屏幕驱动插件'
 rm -rf package/lean/k3screenctrl
-git clone https://github.com/je668/k3screenctrl_build.git package/lean/k3screenctrl/
+git clone https://github.com/JE668/k3screenctrl_build.git package/lean/k3screenctrl/
 echo '=========Replace k3screen drive plug OK!========='
 
-# echo '移除bcm53xx中的其他机型'
-# sed -i '421,453d' target/linux/bcm53xx/image/Makefile
-# sed -i '140,412d' target/linux/bcm53xx/image/Makefile
-# sed -i 's/$(USB3_PACKAGES) k3screenctrl/luci-app-k3screenctrl/g' target/linux/bcm53xx/image/Makefile
+echo '添加OpenClash'
+rm -rf package/lean/luci-app-openclash
+mkdir package/luci-app-openclash
+cd package/luci-app-openclash
+git init
+git remote add -f origin https://github.com/vernesong/OpenClash.git
+git config core.sparsecheckout true
+echo "luci-app-openclash" >> .git/info/sparse-checkout
+git pull --depth 1 origin master
+git branch --set-upstream-to=origin/master master
+# git clone https://github.com/vernesong/OpenClash package/lean/luci-app-openclash
+echo '=========Add OpenClash source OK!========='
+
+# echo '添加Haiibo软件源'
+# sed -i '1i src-git haiibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
+# echo '=========Add Haiibo source OK!========='
+
+# echo '添加kenzok8 openwrt-package'
+# sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+# echo '=========Add kenzok8 source OK!========='
+
+# echo '添加kenzok8 small-package'
+# sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+# echo '=========Add kenzok8 source OK!========='
+
+# echo '添加MosDNS'
+# rm -rf feeds/packages/net/v2ray-geodata
+# rm -rf feeds/haiibo/luci-app-mosdns
+# rm -rf feeds/haiibo/mosdns
+# rm -rf package/feeds/haiibo/luci-app-mosdns
+# rm -rf package/feeds/haiibo/mosdns
+# find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+# find ./ | grep Makefile | grep mosdns | xargs rm -f
+# git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+# git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+# echo '=========Add MosDNS OK!========='
+
+echo '移除bcm53xx中的其他机型'
+sed -i '421,453d' target/linux/bcm53xx/image/Makefile
+sed -i '140,412d' target/linux/bcm53xx/image/Makefile
+sed -i 's/$(USB3_PACKAGES) k3screenctrl/luci-app-k3screenctrl/g' target/linux/bcm53xx/image/Makefile
 # sed -n '140,146p' target/linux/bcm53xx/image/Makefile
-# echo '=========Remove other devices of bcm53xx OK!========='
+echo '=========Remove other devices of bcm53xx OK!========='
 
 # 1.'asus_dhd24' 2.'ac88u_20' 3.'69027'
 # firmware='69027'
 # echo '替换无线驱动'
-# wget -nv https://github.com/je668/Phicomm-k3-Wireless-Firmware/raw/master/brcmfmac4366c-pcie.bin.${firmware} -O /package/lean/k3-firmware/files/brcmfmac4366c-pcie.bin
+# wget -nv https://github.com/JE668/Phicomm-k3-Wireless-Firmware/raw/master/brcmfmac4366c-pcie.bin.${firmware} -O package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
 # echo '=========Replace k3 wireless firmware OK!========='
