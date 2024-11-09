443720003910df266e9be7cb0842a7a6d0eb2c9b
JE668 <63463703+JE668@users.noreply.github.com>
2024-11-09 08:58:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -112,7 +112,7 @@ echo '=========Replace k3screen drive plug OK!========='
 # echo '=========Remove other devices of bcm53xx OK!========='
 
 # 1.'asus_dhd24' 2.'ac88u_20' 3.'69027'
-firmware='69027'
-echo '替换无线驱动'
-wget -nv https://github.com/je668/Phicomm-k3-Wireless-Firmware/raw/master/brcmfmac4366c-pcie.bin.${firmware} -O /package/lean/k3-firmware/files/brcmfmac4366c-pcie.bin
-echo '=========Replace k3 wireless firmware OK!========='
+# firmware='69027'
+# echo '替换无线驱动'
+# wget -nv https://github.com/je668/Phicomm-k3-Wireless-Firmware/raw/master/brcmfmac4366c-pcie.bin.${firmware} -O /package/lean/k3-firmware/files/brcmfmac4366c-pcie.bin
+# echo '=========Replace k3 wireless firmware OK!========='
